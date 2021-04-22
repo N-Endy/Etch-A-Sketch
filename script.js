c91b0d25f5b6ca0dfd9142cf18895e0794756cd9
N-Endy <okafornel@gmail.com>
2021-04-22 17:08:45 +0100

    created two new functions, first to create new divs with user input and the other to clear initial div before new div is created

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// cache DOM for usable variables
 const button = document.querySelector('.btn');
 const container = document.querySelector('.container');
 
-function createGrid(cols, rows) {
+// Function to create initial 16 * 16 grid
+function initialGrid(cols, rows) {
     container.style.setProperty('--grid-cols', cols);
     container.style.setProperty('--grid-rows', rows);
     for (i = 0; i < (rows * cols); i++) {
@@ -9,5 +11,34 @@ function createGrid(cols, rows) {
         container.appendChild(cell).className = "grid";
     };
 }
+initialGrid(16, 16);
 
-createGrid(100, 100);
+// Button Event Listener to collect user Input
+button.addEventListener('click', () => {
+    let userInput;
+    do {
+        userInput = prompt('Please input a number for the divs', '');
+    } while (userInput > 100 || userInput === '');
+    // call external function to apply the user input
+    createGrid(userInput, userInput);
+    
+})
+
+function createGrid(col, row) {
+    // clear initial div created
+    removeChildNodes();
+
+    // create new divs with user input
+    container.style.setProperty('--grid-cols', col);
+    container.style.setProperty('--grid-rows', row);
+    for (i = 0; i < (col * row); i++) {
+        let cell = document.createElement("div");
+        container.appendChild(cell).className = "grid";
+    };
+}
+
+function removeChildNodes() {
+    let divs = document.querySelectorAll('.grid');
+    console.log(divs)
+    divs.forEach(div => container.removeChild(div));
+}
